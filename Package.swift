f9b84d5ca1db9a6cb6d137684d584edaf22b1c1d
iyatsouba <ivan.yatsouba@gmail.com>
2023-10-09 23:36:42 +0300

    init

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "CartoMobileSDK",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "CartoMobileSDK",
+            targets: ["CartoMobileSDK"]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CartoMobileSDK",
+            url: "https://nutifront.s3.amazonaws.com/sdk_snapshots/sdk4-ios-4.4.7-devel.zip",
+            checksum: "01737b357e328176c85307f3bb946b1f8fb1de923c5e5fef307b8a16f34aef35"
+        )
+    ]
+)
