d3ea1696c6d1081fac175e9c3e6b8ba17d7cfaba
Jenkins_Bot <ios.team.mindigital@gmail.com>
2023-11-14 16:27:35 +0200

    diia datasource and layer

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .binaryTarget(
             name: "CartoMobileSDK",
             url: "https://raw.githubusercontent.com/yatsoubaI/CartoMobileSDK/main/sdk4-ios-4.4.7-devel.zip",
-            checksum: "f4482518abf4b475459e1ce379f5824c31a6348968e11ebacf00a1f08d1c3230"
+            checksum: "6087a49328c542434abb6e8337435aba6cf921d5eef5da07edb6171fde138095"
         )
     ]
 )
